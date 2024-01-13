8c886171ebf3695f0daeaa05c9688a49b9619655
Fentahun/Mekonnen <87775777+Ajibarew@users.noreply.github.com>
2024-01-12 22:57:46 -0600

    create signin api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -29,6 +30,7 @@ export default function SignUp() {
         setError(true);
         return;
       }
+      navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(true);
@@ -59,19 +61,21 @@ export default function SignUp() {
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
+      
         <button
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-      </form>
-      <div className="flex gap-2 mt-5">
-        <p>Have an account?</p>
+        <div className="flex gap-2 mt-5">
+        <p>Already have an account?</p>
         <Link to="/sign-in">
-          <span className="text-blue-500">Sign in</span>
+          <span className="text-blue-500">Log In</span>
         </Link>
       </div>
+      </form>
+     
       <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
     </div>
   );
